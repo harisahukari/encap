dfdfc28b2a9a2a206a1299beebf8d454507cf898
harisahukari <sahukarihari3@gmail.com>
2023-09-27 16:41:56 +0530

    six commit

---
+++ src/encap/Main.java
@@ -0,0 +1,13 @@
+package encap;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cap1 c = new Cap1();
+		c.setId(1234);
+		c.setName("Hari");
+		System.out.println("Id is :"+c.getId());
+		System.out.println("Name is :"+c.getName());
+	}
+
+}
